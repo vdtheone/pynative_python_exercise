19afb5b21b688a654cc6d9f1c28480dcd1a7c9a2
vdtheone <vishalprajapati22052001@gmail.com>
2023-12-16 09:28:17 +0530

    Display three string “Name”, “Is”, “James” as “Name**Is**James”

---
+++ Python_Input_Output_Exercise/Exercise_2.py
@@ -0,0 +1,5 @@
+# Display three string “Name”, “Is”, “James” as “Name**Is**James”
+
+# For example: print('Name', 'Is', 'James') will display Name**Is**James
+
+print('My', 'Name', 'Is', 'James', sep="**")
