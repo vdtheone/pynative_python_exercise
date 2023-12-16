cefab5e26443ee4644166b40d6beb69efa5ae432
vdtheone <vishalprajapati22052001@gmail.com>
2023-12-16 09:29:41 +0530

    Write a program to take three names as input from a user in the single input() function call.

---
+++ Python_Input_Output_Exercise/Exercise_7.py
@@ -0,0 +1,7 @@
+# Write a program to take three names as input from a user in the single input() function call.
+
+
+name1, name2, name3 = input("enter three name separated by space = ").split()
+print(name1)
+print(name2)
+print(name3)
