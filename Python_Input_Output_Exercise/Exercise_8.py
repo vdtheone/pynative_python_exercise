76afa058ec926be385ad5c499ac5010d47e12495
vdtheone <vishalprajapati22052001@gmail.com>
2023-12-16 09:29:51 +0530

    Write a program to use string.format() method to format the following three variables as per the expected output

---
+++ Python_Input_Output_Exercise/Exercise_8.py
@@ -0,0 +1,9 @@
+# Write a program to use string.format() method to format the following three variables as per the expected output
+
+totalMoney = 1000
+quantity = 3
+price = 450
+
+data = "I have {0} dollars so I can buy {1} football for {2} dollars."
+
+print(data.format(totalMoney, quantity, price))
