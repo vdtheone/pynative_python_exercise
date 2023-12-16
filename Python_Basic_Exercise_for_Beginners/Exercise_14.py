216db9b0a2ea132eaad6734dc128ac8387555c7b
vdtheone <vishalprajapati22052001@gmail.com>
2023-12-16 09:26:31 +0530

    Print a downward Half-Pyramid Pattern of Star

---
+++ Python_Basic_Exercise_for_Beginners/Exercise_14.py
@@ -0,0 +1,13 @@
+# Print a downward Half-Pyramid Pattern of Star
+
+# * * * * *  
+# * * * *  
+# * * *  
+# * *  
+# *
+
+
+for i in range(5):
+    for j in range(5-i):
+        print("*", end=" ")
+    print(" ")
