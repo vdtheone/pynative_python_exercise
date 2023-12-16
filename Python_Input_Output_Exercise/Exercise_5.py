5972c6b4fe5537f6e32db4bddc4092abfc9dfbe8
vdtheone <vishalprajapati22052001@gmail.com>
2023-12-16 09:29:09 +0530

    Accept a list of 5 float numbers as an input from the user

---
+++ Python_Input_Output_Exercise/Exercise_5.py
@@ -0,0 +1,11 @@
+# Accept a list of 5 float numbers as an input from the user
+
+float_list = []
+
+for i in range(5):
+    num = float(input("Enter number = "))
+    float_list.append(num)
+
+
+print(float_list)
+    
