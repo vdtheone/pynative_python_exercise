69546dca7f2e2931666ed9850baea3a272afb168
vdtheone <vishalprajapati22052001@gmail.com>
2023-12-20 09:55:33 +0530

    Calculate the cube of all numbers from 1 to a given number

---
+++ Python_if_else_for_loop_range/Exercise_16.py
@@ -0,0 +1,6 @@
+# Calculate the cube of all numbers from 1 to a given number
+
+num = int(input("Enter number = "))
+
+for i in range(1,num+1):
+    print(f"Current Number is : {i} and the cube is {i**3}")
