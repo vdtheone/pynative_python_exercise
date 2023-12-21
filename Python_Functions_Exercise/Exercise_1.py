e7d7aa4232ca84c641a393eb0a850cfb849982eb
vdtheone <vishalprajapati22052001@gmail.com>
2023-12-21 16:42:14 +0530

    create a function that takes two arguments, name and age, and print their value.

---
+++ Python_Functions_Exercise/Exercise_1.py
@@ -0,0 +1,9 @@
+# Write a program to create a function that takes two arguments, 
+# name and age, and print their value.
+
+
+def details(name, age):
+    print("Name = ", name, "Age = ", age)
+
+
+details("vishal", 22)
