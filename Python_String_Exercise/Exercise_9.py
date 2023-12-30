4834f4e25ac89ff1a6eb798aba9a2250a5eefb19
vdtheone <vishalprajapati22052001@gmail.com>
2023-12-30 23:32:46 +0530

    Find all occurrences of a substring in a given string by ignoring the case

---
+++ Python_String_Exercise/Exercise_9.py
@@ -0,0 +1,7 @@
+# Find all occurrences of a substring in a given string by ignoring the case
+
+# Write a program to find all occurrences of “USA” in a given string ignoring the case.
+
+str1 = "Welcome to USA. usa awesome, isn't it?"
+
+print((str1.upper().count("USA")))
