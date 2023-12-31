f038d970b2c61720f4f0554fed15977405efb3a1
vdtheone <vishalprajapati22052001@gmail.com>
2023-12-31 17:24:08 +0530

    Find the last position of a given substring

---
+++ Python_String_Exercise/Exercise_13.py
@@ -0,0 +1,7 @@
+# Exercise 12: Find the last position of a given substring
+# Write a program to find the last position of a substring “Emma” in a given string.
+
+
+str1 = "Emma is a data scientist who knows Python. Emma works at google."
+
+print(str1.rfind("Emma"))
