16802b542ed47e6325aa506c959e798104061800
vdtheone <vishalprajapati22052001@gmail.com>
2023-12-31 17:24:20 +0530

    Split a string on hyphens

---
+++ Python_String_Exercise/Exercise_14.py
@@ -0,0 +1,9 @@
+# Exercise 13: Split a string on hyphens
+# Write a program to split a given string on hyphens and display each substring.
+
+str1 = "Emma-is-a-data-scientist"
+
+data = str1.split("-")
+
+for i in data:
+    print(i)
