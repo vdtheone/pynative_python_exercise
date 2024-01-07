4c111e7836ae6a8c8c34e61f73582789bca0797b
vdtheone <vishalprajapati22052001@gmail.com>
2024-01-07 23:08:24 +0530

    Merge two Python dictionaries into one

---
+++ Python_Dictionary_Exercise/Exercise_2.py
@@ -0,0 +1,11 @@
+# Merge two Python dictionaries into one
+
+dict1 = {'Ten': 10, 'Twenty': 20, 'Thirty': 30}
+dict2 = {'Thirty': 30, 'Fourty': 40, 'Fifty': 50}
+
+dict3 = {**dict1, **dict2}
+print(dict3)
+
+dict4 = dict1.copy()
+dict4.update(dict2)
+print(dict4)
