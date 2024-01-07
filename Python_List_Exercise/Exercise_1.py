c30f82bb90390e9ba3947f31c3b51ec9e3e3c71f
vdtheone <vishalprajapati22052001@gmail.com>
2024-01-07 23:09:11 +0530

    Reverse a list in Python

---
+++ Python_List_Exercise/Exercise_1.py
@@ -0,0 +1,9 @@
+# Reverse a list in Python
+
+list1 = [100, 200, 300, 400, 500]
+
+print("Reverse list = ", list1[::-1])
+print("Reverse list = ", list(reversed(list1)))
+
+list1.reverse()
+print("Reverse list = ", list1)
