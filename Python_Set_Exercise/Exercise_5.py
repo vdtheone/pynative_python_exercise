4de8942290111b6ed8a8a33f40a09deb0351ca84
vdtheone <vishalprajapati22052001@gmail.com>
2024-01-11 17:53:47 +0530

    Remove items from the set at once

---
+++ Python_Set_Exercise/Exercise_5.py
@@ -0,0 +1,9 @@
+# Remove items from the set at once
+
+# Write a Python program to remove items 10, 20, 30 from the following set at once.
+
+set1 = {10, 20, 30, 40, 50}
+
+set1.difference_update({10,20,30})
+
+print(set1)
