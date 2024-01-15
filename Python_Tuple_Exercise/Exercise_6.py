477b0be48c436525598b8359e85f673b5d23482c
vdtheone <vishalprajapati22052001@gmail.com>
2024-01-15 09:44:07 +0530

    Copy specific elements from one tuple to a new tuple

---
+++ Python_Tuple_Exercise/Exercise_6.py
@@ -0,0 +1,11 @@
+#  Copy specific elements from one tuple to a new tuple
+
+# Write a program to copy elements 44 and 55 from the following tuple into a new tuple.
+
+tuple1 = (11, 22, 33, 44, 55, 66)
+
+tuple2 = tuple1[3:5]
+tuple3 = tuple1[3:-1]
+
+print(tuple2)
+print(tuple3)
