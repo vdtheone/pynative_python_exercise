808baa1a7e29ae3f4a6d24ca139d24e674c673c1
vdtheone <vishalprajapati22052001@gmail.com>
2024-01-27 19:47:31 +0530

    Find the day of the week of a given date

---
+++ Python_Date_and_Time_Exercise/Exercise_5.py
@@ -0,0 +1,10 @@
+# Find the day of the week of a given date
+
+from datetime import datetime
+
+
+given_date = datetime(2020, 7, 26)
+
+print(given_date.today().weekday())
+
+print(given_date.strftime("%A"))
