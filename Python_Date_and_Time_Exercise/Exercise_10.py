2ba72781bbdf019fc603d62cd05c406c8d55ff1a
vdtheone <vishalprajapati22052001@gmail.com>
2024-02-09 00:23:00 +0530

    Calculate number of days between two given dates

---
+++ Python_Date_and_Time_Exercise/Exercise_10.py
@@ -0,0 +1,17 @@
+# Calculate number of days between two given dates
+
+from datetime import datetime
+
+# 2020-02-25
+date_1 = datetime(2020, 2, 25).date()
+# 2020-09-17
+date_2 = datetime(2020, 9, 17).date()
+
+delta = None
+if date_1 > date_2:
+    print("date_1 is greater")
+    delta = date_1 - date_2
+else:
+    print("date_2 is greater")
+    delta = date_2 - date_1
+print("Difference is", delta.days, "days")
